4de11730a3ccfecc05b20782453ab7a482149bc5
chaeyeonan <dkscodus1204@ewhain.net>
2022-12-13 10:48:51 +0900

    ✨ sql query

--- php/insert_user_data.php
+++ php/insert_user_data.php
@@ -1,6 +1,7 @@
 <?php
 
   // $_POST 변수에서 받는 키워드는 input 태그의 name값이다
+  // CRUD : Created, Read, Updae, Delete
 
   $name = $_POST['name'];
   $id = $_POST['id'];
@@ -16,28 +17,42 @@
   // ------- sql 문법 위추로 파악 --------
   // 1. 데이터 삽입 : INSERT INTO [tablename] (column1, column2...) VALUES (column value1, column value2...);
   // - 첫번째 괄호 안과 두번째 괄호 안의 개수와 순서가 모두 맞아야 한다
-//   $sql = "INSERT INTO spl_user (
-//     user_name,
-//     user_id,
-//     user_email,
-//     user_pw
-//   ) VALUES (
-//     '$name',
-//     '$id',
-//     '$email',
-//     '$pwd'
-//   )";
+  $sql = "INSERT INTO spl_user (
+    user_name,
+    user_id,
+    user_email,
+    user_pw
+  ) VALUES (
+    '$name',
+    '$id',
+    '$email',
+    '$pwd'
+  )";
 
-  // 2. 데이터 조회 : SELECT field name(* : 전체조회 시) FROM table name;
+  // 2. 데이터 조회 : SELECT [field name(* : 전체조회 시)] FROM [table name];
   // - 첫번째 괄호 안과 두번째 괄호 안의 개수와 순서가 모두 맞아야 한다
   // 데이터 조회 시 특정 필드만 조회하는 경우 필드명을, 전체 조회할 경우 *를 사용한다.
+  $sql = "SELECT user_name FROM spl_user";
   $sql = "SELECT * FROM spl_user";
+
+  // 3. 데이터 조회 필터링 : SELECT [field name] FROM [table name] WEHRE [field name] = [field value]
+  $sql = "SELECT * FROM spl_user WHERE user_idx=2";
+
+  // 4. 데이터 정렬 : SELECT [field name] FROM [table name] ORDER BY [field name] DESC(연순) or ASC (정순))
+  $sql = "SELECT * FROM spl_user ORDER BY user_idx DESC";
+
+  // 5. 데이터  개수 지정 조회 : SELECT [field name] FROM [table name] LIMIT [number][start index],[number][length]]
+  $sql = "SELECT * FROM spl_user LIMIT 1,3";
+  
+  // 6. 데이터 값 변경 (update) :  UPDATE [table name] SET [field name]=[field  value] WHERE [field name]=[field  value]
+  // WHERE   필터링 없이 업데이트만 한다면
+  $sql = "UPDATE spl_user SET user_name='babo' WHERE user_idx=2";
+  
+  // 7. 데이터 삭제 (delete) : DELETE FROM [table name] WHERE [field name]=[field value];
+  $sql="DELETE FROM spl_user WHERE user_idx=2 and user_name='babo'"
 
   // mysqli_query() : 데이터베이스테 작업을 요청하는 함수 : 첫번째 파라미터에는 접속정보, 두번째 파라미터에는 요청 작업을 전달
   $res = mysqli_query($db_conn, $sql);
-
-  // echo var_dump($result);
-  // echo $result;
 
   // echo "
   //   <script>
@@ -50,12 +65,12 @@
 
   while($row = mysqli_fetch_array($res)){
     $user_idx = $row['user_idx'];
-    $user_id = $row['user_id'];
+   $user_name = $row['user_name'];
   
 ?>
 
     <p>회원번호 : <?=$user_idx?></p>
-    <p>아이디 : <?=$user_id?></p>
+    <p> 이름  : <?=$user_name?> </p>
 
 <?php
   }
